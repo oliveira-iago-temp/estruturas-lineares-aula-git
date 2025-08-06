8052363a9cb175a6c452a7f6ac159530ff271011
aluno cmc <alunocmc@umc.br>
2025-08-06 19:29:10 -0300

    aula git

---
+++ aula-git/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe seu nome:");
+        String nome = teclado.nextLine();
+        System.out.println("nome:" + nome);
+
+
+
+
+    }
+}
