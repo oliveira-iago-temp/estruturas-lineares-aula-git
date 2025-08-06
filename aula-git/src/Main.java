cf6d3abfaa34e366d1a857d6ad4395fae5b7d6b4
aluno cmc <alunocmc@umc.br>
2025-08-06 20:08:14 -0300

    :monkey: Alterações nos textos de input

--- aula-git/src/Main.java
+++ aula-git/src/Main.java
@@ -2,15 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Informe seu nome:");
+        System.out.print("\nInforme seu nome: ");
         String nome = teclado.nextLine();
-        System.out.println("nome:" + nome);
-
-
-
-
+        System.out.println("Bem-vindo " + nome + "!");
     }
 }
